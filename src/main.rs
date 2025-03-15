7d8836d617b3db582aeceef0fe5a549480b2dd83
sudeeptarlekar <sudeeptarlekar@gmail.com>
2025-03-15 20:45:10 +0100

    Added error message

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use anyhow::Context;
 use anyhow::Result;
 use git2::Repository;
 
 fn main() -> Result<()> {
-    let repo = Repository::open(".")?;
+    let repo = Repository::open(".").context("Current working directory is not Git Directory")?;
 
     println!("{:?}", repo.path());
     Ok(())

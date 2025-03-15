25fbf403e321e83857655679459be556c7d85ee8
sudeeptarlekar <sudeeptarlekar@gmail.com>
2025-03-15 20:44:04 +0100

    Added repo selection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use git2::Repository;
+
+fn main() -> Result<()> {
+    let repo = Repository::open(".")?;
+
+    println!("{:?}", repo.path());
+    Ok(())
 }
